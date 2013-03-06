bad2469b44d7b59a87832cc31963e00ea7f6621a
Marcin Grzybowski <m.j.grzybowski@gmail.com>
2013-03-06 20:09:53 +0100

    dziala pierwsza wersja dodawania spanow

--- dataLiner.js
+++ dataLiner.js
@@ -14,17 +14,29 @@ var dataLiner = function() {
     text = document.getElementById("mw-content-text").innerHTML;
     var regExp1 = /\u0020\d{4,4}[-|–]\d{4,4}\u0020/igm;
     var regExp2 = /\u0020[1-2][0-9][0-9][0-9]\u0020/igm;
-    var regExp3 = /\u0020\d{4,4}\u0020/igm;
+    var regExp3 = /\u0020\d{4,4}\u0020/g;
 
     var match, output;
 
-    while ((match = text.match(regExp3)) != null) {
 
-        for(var i = match.length; i >0; i--){
-            output = [text.slice(0, match[i].index+4), "\</span>", text.slice(match[i].index+4)].join('');
-            output = [text.slice(0, match[i].index), "\<span style=\"color: red;\">", text.slice(match[i].index)].join('');
-        }
+
+    var matchAll = [];
+    while ((match = regExp3.exec(text)) != null) {
+        //console.log(match);
+        matchAll.push(match);
+
     }
+    console.log(matchAll);
+
+    var sString = '</span>';
+    var bString = '<span style="color:red">';
+
+    output = text;
+
+    for(var i = matchAll.length-1; i>=0; i--){
+        output = [output.slice(0,matchAll[i].index+5), sString, output.slice(matchAll[i].index+5)].join('');
+        output = [output.slice(0,matchAll[i].index), bString, output.slice(matchAll[i].index)].join('');
+     }
     document.getElementById("mw-content-text").innerHTML = output;
 };
 

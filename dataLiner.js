b2b782557aab89d76ca0e40b69f56868de770a4f
KamilWo <kamil.m.wozniak+git@gmail.com>
2013-03-06 19:19:10 +0100

    output

--- dataLiner.js
+++ dataLiner.js
@@ -5,18 +5,28 @@
  * Time: 16:43
  * To change this template use File | Settings | File Templates.
  */
+/*var re1 = /\d+/g;*/
+/*var re1 = /^([1-9]|[1-9][0-9]|[1-9][0-9][0-9])$;*/
 
 var text;
 
 var dataLiner = function() {
-    text = document.getElementById("mw-content-text").textContent;
-    var re1 = /\d{4,4}/g;
-    /*var re1 = /\d+/g;*/
-    /*var re1 = ^([1-9]|[1-9][0-9]|[1-9][0-9][0-9])$;*/
-    var t = text.match(re1);
-    /*document.write(t);*/
-    alert("done");
-    return t;
+    text = document.getElementById("mw-content-text").innerHTML;
+    var regExp1 = /\u0020\d{4,4}[-|–]\d{4,4}\u0020/igm;
+    var regExp2 = /\u0020[1-2][0-9][0-9][0-9]\u0020/igm;
+    var regExp3 = /\u0020\d{4,4}\u0020/igm;
+
+    var match, output;
+
+    while ((match = text.match(regExp3)) != null) {
+
+        for(var i = match.length; i >0; i--){
+            output = [text.slice(0, match[i].index+4), "\</span>", text.slice(match[i].index+4)].join('');
+            output = [text.slice(0, match[i].index), "\<span style=\"color: red;\">", text.slice(match[i].index)].join('');
+        }
+    }
+    document.getElementById("mw-content-text").innerHTML = output;
 };
+
 
 dataLiner();
